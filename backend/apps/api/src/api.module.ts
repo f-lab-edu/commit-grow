5b9b94fcde12588786e1897a1119504508bf9e2d
juice0119 <3220ju@naver.com>
2026-07-01 14:13:16 +0900

    chore(backend): Biome로 코드 포맷터 및 lint prettier 제거
    ㅇㅇ

--- backend/apps/api/src/api.module.ts
+++ backend/apps/api/src/api.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
-import { ApiService } from './api.service';
+import { Module } from "@nestjs/common";
+import { ApiController } from "./api.controller";
+import { ApiService } from "./api.service";
 
 @Module({
-  imports: [],
-  controllers: [ApiController],
-  providers: [ApiService],
+	imports: [],
+	controllers: [ApiController],
+	providers: [ApiService],
 })
 export class ApiModule {}
