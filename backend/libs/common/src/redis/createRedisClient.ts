1f5418a446c5ebd5722addd7cb1535d9a92e1044
juice0119 <3221ju@gmail.com>
2026-07-10 07:55:07 +0900

    feat(api): Redis 연결 최대 재시도 횟수 설정 추가

--- backend/libs/common/src/redis/createRedisClient.ts
+++ backend/libs/common/src/redis/createRedisClient.ts
@@ -1,18 +1,25 @@
 import { createClient, type RedisClientType } from 'redis';
+import { SystemException } from '../exception/SystemException';
 
-const MAX_REDIS_CONNECT_RETRIES = 5;
+const DEFAULT_MAX_REDIS_CONNECT_RETRIES = 5;
 
 export function createRedisClient(options: {
 	host: string;
 	port: number;
+	maxConnectRetries: number;
 }): RedisClientType {
+	const maxConnectRetries =
+		options.maxConnectRetries || DEFAULT_MAX_REDIS_CONNECT_RETRIES;
+
 	return createClient({
 		socket: {
 			host: options.host,
 			port: options.port,
 			reconnectStrategy: (retries) =>
-				retries > MAX_REDIS_CONNECT_RETRIES
-					? new Error('Redis 연결 재시도 횟수를 초과했습니다')
+				retries > maxConnectRetries
+					? new SystemException(
+							`Redis 연결 최대 재시도 횟수 ${maxConnectRetries} 를 초과했습니다`,
+						)
 					: Math.min(retries * 200, 2000),
 		},
 	});
