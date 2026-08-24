9b7e4c907d0f1559f38d98582a01a90b0e07a631
juice0119 <3221ju@gmail.com>
2026-08-24 19:22:19 +0900

    refactor(domain): Retrospect gitActivities OneToMany lazy 옵션 제거

--- backend/libs/entity/src/domain/Retrospect.entity.ts
+++ backend/libs/entity/src/domain/Retrospect.entity.ts
@@ -56,7 +56,6 @@ export class Retrospect extends BaseTimeEntity {
 	@OneToMany(
 		() => GitActivity,
 		(gitActivity) => gitActivity.retrospect,
-		{ lazy: true },
 	)
 	gitActivities: GitActivity[];
 
